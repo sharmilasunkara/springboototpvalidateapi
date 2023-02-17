1568b6519be1ed2ce4af1910f85077bf0ada1f99
sai <3nadhananth@gmail.com>
2023-02-18 00:37:20 +0530

    First commit

---
+++ Spring_Twilio_Otp_Validate-main/SpringTwilio/src/main/java/Spring/Twilio/Service/PhoneverificationService.java
@@ -0,0 +1,54 @@
+package Spring.Twilio.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.twilio.exception.ApiException;
+import com.twilio.rest.verify.v2.service.Verification;
+import com.twilio.rest.verify.v2.service.VerificationCheck;
+
+import Spring.Twilio.Twilioproperties;
+
+@Service
+public class PhoneverificationService {
+
+	private final Twilioproperties twilioproperties;
+
+	@Autowired
+	public PhoneverificationService(Twilioproperties twilioproperties) {
+		this.twilioproperties=twilioproperties;
+	}
+	
+	
+	//method to send to otp
+    public VerificationResult startVerification(String phone) {
+        try {
+            Verification verification = Verification.creator(twilioproperties.getServiceId(), phone, "sms").create();
+            if("approved".equals(verification.getStatus())|| "pending".equals(verification.getStatus())) {
+			return new VerificationResult(verification.getSid());
+			}
+        } catch (ApiException exception) {
+            return new VerificationResult(new String[] {exception.getMessage()});
+        }
+        return null;
+    }
+
+    //mehtod to verifiy the otp
+    public VerificationResult checkverification(String phone, String code) {
+        try {
+           // VerificationCheck verification = VerificationCheck.creator(twilioproperties.getServiceId(), code).setTo(phone).create();
+        	VerificationCheck verificationCheck = VerificationCheck.creator(
+        			twilioproperties.getServiceId())
+                .setTo(phone)
+                .setCode(code)
+                .create();
+        	if("approved".equals(verificationCheck.getStatus())) {
+                return new VerificationResult(verificationCheck.getSid());
+            }
+            return new VerificationResult(new String[]{"Invalid code."});
+        } catch (ApiException exception) {
+            return new VerificationResult(new String[]{exception.getMessage()});
+        }
+    }
+	
+}
